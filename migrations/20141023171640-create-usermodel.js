b4a35f716d3c7c865c8ede4cdee206a761cd528b
MPMoughan <MattM@amandobs-mbp-2.attlocal.net>
2014-11-27 16:35:44 -0800

    first deploy attempt

---
+++ migrations/20141023171640-create-usermodel.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    }).complete(done);
+  },
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    migration.dropTable("users").complete(done);
+  }
+};
---
+++ public/javascripts/globe_tweet.js
@@ -0,0 +1,91 @@
+var globeTweet = {};
+globeTweet.map = {};
+
+globeTweet.renderMap = function() {
+  // render map
+  L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGhld3AiLCJhIjoiX1RLYm1aNCJ9.3qL5nUQgGoVPOYzMDFNVcg';
+  globeTweet.map = L.mapbox.map('map', 'matthewp.k96jnj38', {
+    center: [38, -95],
+    zoom: 3,
+    minZoom: 2,
+  });
+};
+
+globeTweet.startStream = function() {
+  // set variables
+  var socket = io(),
+    tweetDiv = $('#tweetd'),
+    count = 0,
+    geocoder = L.mapbox.geocoder('mapbox.places-v1'),
+    counter = $('#counter'),
+    loadMessage = $('#load-msg'),
+    tweetCount = $('#tweet-count'),
+    waitMessage = $('#wait-msg');
+  console.log ("vars set");
+
+  setTimeout(function() {
+    if (count === 0) {
+      waitMessage.fadeIn();
+    }
+  }, 15000);
+  console.log("set timeout");
+  socket.on('receive_tweet', function(tweet) {
+    console.log ("calling socket on event");
+    // function to show markers on map
+    var showMarker = function(lng, lat) {
+      L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            lng,
+            lat
+          ]
+        },
+        properties: {
+          description: '@' + tweet.user.screen_name + ': ' + tweet.text,
+          'marker-size': 'small',
+          'marker-color': '#FC4607',
+          'marker-symbol': 'star'
+        }
+      }).addTo(globeTweet.map);
+    };
+
+
+    console.log('receiving tweet');
+    tweetDiv.prepend($('<div class="clr"><img src="'
+      + tweet.user.profile_image_url + '" > <strong>@'
+      + tweet.user.screen_name + ':</strong> <a href="http://twitter.com/'
+      + tweet.user.screen_name + '/status/'
+      + tweet.id_str + '" target="blank">'
+      + tweet.text + '</a></div>').fadeIn('slow','swing'));
+
+    count += 1;
+    counter.html(count);
+
+    if (count > 0) {
+      loadMessage.hide();
+      waitMessage.hide();
+      tweetCount.show();
+    }
+
+    if (tweet.geo) {
+      showMarker(tweet.geo.coordinates[1], tweet.geo.coordinates[0]);
+    }
+
+    else if (tweet.user.location) {
+      geocoder.query(tweet.user.location, function(err, result) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          showMarker(result.latlng[1], result.latlng[0]);
+        }
+      });
+    }
+
+  });
+};
+
+
+
